664654dba71363bcf83614ca5cf7e491b4ba7148
matheushcamilo <matheushcamilo1997@gmail.com>
2021-09-13 15:13:04 -0300

    Queue class added

--- src/main/java/com/estruturasdedados/Queue/Main.java
+++ src/main/java/com/estruturasdedados/Queue/Main.java
@@ -1,5 +1,25 @@
 package com.estruturasdedados.Queue;
 
+import com.estruturasdedados.Stack.Node;
+
 public class Main {
+    public static void main(String[] args) {
+        Queue fila = new Queue();
+        Node no1 = new Node("teste");
+        Node no2 = new Node(54);
+        Node no3 = new Node(23);
+        Node no4 = new Node(80);
+        fila.enqueue(no1);
+        fila.enqueue(no2);
+        fila.enqueue(no3);
+        fila.enqueue(no4);
+        System.out.println(fila.toString());
+        System.out.println(fila.first().toString());
+        System.out.println(fila.dequeue().toString());
+        System.out.println(fila.dequeue().toString());
+        System.out.println(fila.dequeue().toString());
+        System.out.println(fila.dequeue().toString());
+    }
+
 
 }
--- src/main/java/com/estruturasdedados/Queue/Queue.java
+++ src/main/java/com/estruturasdedados/Queue/Queue.java
@@ -1,5 +1,59 @@
 package com.estruturasdedados.Queue;
 
+import com.estruturasdedados.Stack.Node;
+
 public class Queue {
+    private Node head;
+
+    public Queue() {
+        this.head = null;
+    }
+    boolean isEmpty(){
+        return head == null ? true : false;
+    }
+
+    void enqueue(Node node) {
+        node.setRef(head);
+        head = node;
+    }
+    Node first(){
+        Node aux = head;
+        while(aux.getRef()!=null){
+            aux = aux.getRef();
+        }
+        return aux;
+    }
+
+    Node dequeue() {
+        if(!isEmpty()){
+            Node aux = head;
+            Node aux2 = aux;
+            while (aux.getRef() != null) {
+                aux2 = aux;
+                aux = aux.getRef();
+            }
+            aux2.setRef(null);
+            return aux;
+        }
+        System.out.println("Stack Underflow!!!");
+        return null;
+    }
+
+    public String toString() {
+        String str = "-----------\n";
+        str += "Queue\n";
+        str += "-----------\n";
+        Node aux = head;
+        while (true) {
+            if (aux.getRef() == null){
+                str += "{" + aux.getValue() + "}\n";
+                break;
+            }
+            str += "{" + aux.getValue() + "}\n";
+            aux = aux.getRef();
+        }
+        return str;
+    }
+
 
 }
