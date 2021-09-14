5c298f260c3202ee625e5bca6277cfa43a399102
matheushcamilo <matheushcamilo1997@gmail.com>
2021-09-13 21:10:06 -0300

    LinkedList class added; Functionalities must undergo tests

--- src/main/java/com/estruturasdedados/LinkedList/LinkedList.java
+++ src/main/java/com/estruturasdedados/LinkedList/LinkedList.java
@@ -1,5 +1,86 @@
 package com.estruturasdedados.LinkedList;
 
-public class LinkedList {
+import com.estruturasdedados.Stack.Node;
 
+public class LinkedList<T> {
+    Node<T> head;
+
+    public LinkedList() {
+        this.head = null;
+    }
+
+    boolean isEmpty(){return this.head == null ? true: false;}
+
+    int size(){
+        int cont = 0;
+        Node<T> aux = head;
+        while(aux.getRef()!=null){
+            aux = aux.getRef();
+            cont++;
+        }
+        return cont;
+    }
+
+    Node<T> getNode(int index){
+        Node<T> aux = head;
+        Node<T> aux2 = null;
+        if(index >= size()){
+            System.out.println("Não existe tal índice");
+            return null;
+        }
+        for (int i = 0; i <= index; i++) {
+            aux2 = aux;
+            aux = aux.getRef();
+        }
+        return aux2;
+    }
+
+    public T get(int index){
+        return getNode(index).getValue();
+    }
+
+    void add(int index, T valor) {
+        int cont = 0;
+        Node<T> aux;
+        Node<T> aux2;
+        Node<T> node = new Node<>(valor);
+        if(this.isEmpty()){
+            head = node;
+            return;
+        }else{
+            int count = 0;
+            aux = head;
+            while(aux.getRef() != null && count < index){
+                aux2 = aux;
+                aux = aux.getRef();
+                count++;
+            }
+            node.setRef(aux.getRef());
+            aux.setRef(node);
+        }
+    }
+
+    public T remove(int index){
+        Node<T> nodeToRemove = this.getNode(index);
+        if(index == 0){
+            head = nodeToRemove.getRef();
+            return nodeToRemove.getValue();
+        }
+        Node<T> nodeAnterior = getNode(index - 1);
+        nodeAnterior.setRef(nodeToRemove.getRef());
+        return nodeToRemove.getValue();
+    }
+
+    @Override
+    public String toString() {
+        Node<T> aux = head;
+        String str = "-------------\n";
+        str += "LinkedList\n";
+        str += "-------------\n";
+        while(aux!=null){
+            str += "{" + aux.getValue() + "}\n";
+            aux = aux.getRef();
+        }
+        return str;
+    }
 }
---
+++ src/main/java/com/estruturasdedados/LinkedList/Main.java
@@ -0,0 +1,19 @@
+package com.estruturasdedados.LinkedList;
+
+import com.estruturasdedados.Stack.Node;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<>();
+        list.add(0,"teste1");
+        list.add(1,"teste2");
+        list.add(2,"teste3");
+        list.add(3,"teste4");
+
+        System.out.println(list.toString());
+        System.out.println(list.get(2));
+        list.remove(2);
+        System.out.println(list.toString());
+
+    }
+}
